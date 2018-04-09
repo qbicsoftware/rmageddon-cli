a6971aeacaa6a313798f375575fba221ff0c34d7
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-04-09 15:33:23 +0200

    Add docstrings for linting function

--- r_lint/lint.py
+++ r_lint/lint.py
@@ -13,7 +13,31 @@ class RContainerLint(object):
     def __init__(self, pipeline_dir):
         """ Init the linting object """
         self.path = pipeline_dir
+        self.failed = []
+        self.warned = []
+        self.passed = []
+
     
     def lint_rproject(self):
-        pass
+        """ Main linting function.
 
+        This function takes the R project directory and scans 
+        it for different files/dirs and compares it
+        against best practice rules we defined at QBiC for
+        having an interoperable, reproducible R environment
+        for bioinformatic analysis.
+
+        Returns:
+            dict: Summary of test result messages structured as follows:
+            {
+                'pass': [
+                    ( test-id (int), message (string) ),
+                    ( test-id (int), message (string) )
+                ],
+                'warn': [(id, msg)],
+                'fail': [(id, msg)],
+            }
+        
+         Raises:
+            If a critical problem is found, an AssertionError is raised.
+        """
--- tests/test_lint.py
+++ tests/test_lint.py
@@ -12,6 +12,7 @@ Provide example project contents like:
             |--test_lint.py
 """
 import os
+import yaml
 import unittest
 
 import r_lint.lint as lint
@@ -28,8 +29,19 @@ def pf(wd, path):
 WD = os.path.dirname(__file__)
 PATH_MINIMAL_WORKING_EXAMPLE = pf(WD, "lint_examples/minimal_working_example")
 
+# The maximum number of checks that can be passed
+MAX_PASS_CHECKS = 2
+
 class TestLint(unittest.TestCase):
     """ Class for lint tests """
+
+    def assess_lint_status(self, lint_obj, **expected):
+        """Little helper function for assessing the lint
+        object status lists"""
+        for list_type, expect in expected.items():
+            observed = len(getattr(lint_obj, list_type))
+            oberved_list = yaml.safe_dump(getattr(lint_obj, list_type))
+            self.assertEqual(observed, expect, "Expected {} tests in '{}', but found {}.\n{}".format(expect, list_type.upper(), observed, oberved_list))
     
     def test_read_dir_content_and_pass(self):
         """ Check if the dir contains several files/dirs.
@@ -38,3 +50,6 @@ class TestLint(unittest.TestCase):
             Warns if not present: scripts, data
         """
         lint_obj = lint.RContainerLint(PATH_MINIMAL_WORKING_EXAMPLE)
+        lint_obj.lint_rproject()
+        expectations = {"failed": 0, "warned": 0, "passed": MAX_PASS_CHECKS}
+        self.assess_lint_status(lint_obj, **expectations)
