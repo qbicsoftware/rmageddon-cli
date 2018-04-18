09464bf1e7cc0f1d2c03232609c05309eb8d017e
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-04-18 15:55:42 +0200

    Add environment.yml example for test

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,6 +1,14 @@
 #!/usr/env/bin python
 
 import os
+import unittest
 
 def pf(wd, path):
     return os.path.join(wd, path)
+
+class TestBuilder(unittest.TestCase):
+    """ Test class for the r_lint/builder.py code """
+
+    def test_conda_env_parsing_pass(self):
+        """ Parse a conda environment file sucessfully """
+        
