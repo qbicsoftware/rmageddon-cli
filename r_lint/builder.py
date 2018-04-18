634d43525c83348b7aa4cf9785baa5e740824388
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-04-18 18:58:03 +0200

    Fix wrong function call

--- r_lint/builder.py
+++ r_lint/builder.py
@@ -13,6 +13,8 @@ and for 'anaconda' it is:
     bioconductor-<bioconductor pkg name>
 """
 
+import yaml
+
 class EnvBuilder(object):
 
     def __init__(self, rpackages, conda_env):
@@ -20,9 +22,9 @@ class EnvBuilder(object):
         to read a R package list and tries to translate it
         to conda dependencies
         """
-        self.conda_env = conda_env
-        self.rpackages = rpackages
-    
+        self.conda_env = self.parse_conda_env(conda_env)
+        self.rpackages = self.parse_package_list(rpackages)
+
     def build(self):
         """ Takes a list of R packages and tries to find the
         corresponding package in the Anaconda cloud
@@ -30,3 +32,13 @@ class EnvBuilder(object):
         config file
         """
         pass
+    
+    def parse_conda_env(self, conda_env):
+        with open(conda_env, 'r') as stream:
+            data = yaml.load(stream)
+        return data
+
+    def parse_package_list(self, rpackages):
+        with open(rpackages, 'r') as stream:
+            packages = stream.read().splitlines()
+        return packages
