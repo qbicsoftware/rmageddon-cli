b3b0d57b4e91e436db015f8beb628317eeeb9521
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-06-04 14:39:45 +0200

    Adjust tests

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -47,7 +47,7 @@ class TestBuilder(unittest.TestCase):
             pf(PATH_GOOD_CONDA_ENV, "environment.yml")
         )
         builder_obj.check_rpkgs()
-        expectations = {"passed": 0, "warned": 1, "failed": 0}
+        expectations = {"passed": 0, "warned": 2, "failed": 0}
         self.assess_lint_status(builder_obj, **expectations)
     
     def test_rpackage_resolving_and_pass(self):
