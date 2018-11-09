9e5b4a0ffea7ee633cd5b1e64d66e6d50926d08c
Sven F <sven.fillinger@qbic.uni-tuebingen.de>
2018-11-09 11:13:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import sys
 
 version = '0.1.0'
 
@@ -24,6 +25,10 @@ setup(
     license = license,
     scripts = ['scripts/r-lint'],
     install_requires = required,
+    setup_requires=[
+        'twine>=1.11.0',
+        'setuptools>=38.6.',
+    ] + ([] if sys.version_info.minor == 4 else ['wheel>=0.31.0']),
     packages = find_packages(exclude=('docs')),
     include_package_data = True
 )
